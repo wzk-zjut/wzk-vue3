149ba1ef04e351d79b27df4c49e766af377a6fc3
王郑凯 <zjutwzk@163.com>
2020-09-07 16:15:42 +0800

    wzk

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <button @click="toggleModalState">Open Modal</button>
+    <modal v-if="modalOption">
+        <p>Hello, I'm a modal window.</p>
+    </modal>
+</template>
+
+<script>
+import Modal from './views/Modal.vue'
+import { ref } from 'vue'
+
+export default {
+    components: {
+        Modal
+    },
+    setup() {
+        const modalOption = ref(false)
+        const toggleModalState = () => {
+            modalOption.value = !modalOption.value
+        }
+        return {
+            modalOption,
+            toggleModalState
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
